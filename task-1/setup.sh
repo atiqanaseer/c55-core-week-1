1ff5915569bf8cab922732dd609ad765a401ad6d
Atiqa Naseer <atiqa.naseer99@gmail.com>
2026-01-16 23:29:41 +0100

    hyf week 1 assignment

--- task-1/setup.sh
+++ task-1/setup.sh
@@ -1,1 +1,42 @@
-# Write your code here
+echo "Creating project…"
+mkdir project
+git init
+touch project/README.md
+mkdir project/resources
+cd project/resources
+touch "family picture.jpg"
+touch icon.png
+touch logo.png
+cd ../
+touch settings.conf
+mkdir src
+cd src
+mkdir database
+mkdir profile
+touch program.java
+cd ../
+git add .
+git commit -m "initial commit"
+sleep 3
+echo "Setup project…"
+echo "Welcome to my project" >> README.md
+cd src
+rmdir profile
+cd ../
+cd resources 
+rm "family picture.jpg"
+cd ../
+git add .
+git commit -m "second commit"
+sleep 3
+ls resources
+echo "Setup javascript…"
+cd src
+mv program.java program.js
+echo "console.log('JavaScript works!');" >> program.js
+node program.js
+cd ../
+git add .
+git commit -m "third commit"
+ls $HOME
+echo "All done!"
